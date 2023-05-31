26bc55853a728390cfd7e1a7693a01e806c3c39f
Alexey Sokolov <alexvyber@gmail.com>
2023-05-31 16:43:39 +0300

    update tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss/types/config"
 
+import colors from "tailwindcss/colors"
+
 export default {
   content: [
     "./index.html",
@@ -7,7 +9,31 @@ export default {
   ],
   
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.violet,
+        secondary: colors.indigo,
+        danger: colors.red,
+        black: colors.slate,
+        warning: colors.amber,
+        info: colors.blue,
+        success: colors.teal,
+        gray: colors.slate
+      },
+
+      screens: {
+        "3xs": "320px",
+        "2xs": "384px",
+        xs: "512px",
+        sm: "640px",
+        md: "768px",
+        "2md": "896px",
+        lg: "1024px",
+        "2lg": "1152px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
 
   plugins: [],
