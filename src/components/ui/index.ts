253ff75ee0d6423b512efb4fafc66dd0281b2fe2
Alexey Sokolov <alexvyber@gmail.com>
2023-05-31 23:53:23 +0300

    add dialog ui component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,20 +2,28 @@ export { ButtonLink } from "./button-link"
 export { Button } from "./button"
 export { Container } from "./container"
 export { Loading } from "./loading"
-export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "./card"
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card"
 export { Separator } from "./separator"
-export {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from "./accordion"
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./accordion"
 export { Tabs, TabsList, TabsTrigger, TabsContent } from "./tabs"
 export { ScrollArea, ScrollBar } from "./scroll-area"
+export {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "./dialog"
+export {
+  Sheet,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from "./sheet"
+export { Input } from "./input"
