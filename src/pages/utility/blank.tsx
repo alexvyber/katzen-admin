8c08d341a08227c2036d4bfc3db1044fb298271b
Alexey Sokolov <alexvyber@gmail.com>
2023-05-31 12:22:23 +0300

    add 404 and blank pages

---
+++ src/pages/utility/blank.tsx
@@ -0,0 +1,9 @@
+export function Blank() {
+  return (
+    <div>
+      <h4 className="inline-block mb-6 text-xl font-medium capitalize lg:text-2xl text-slate-900">
+        Blank Page
+      </h4>
+    </div>
+  )
+}
