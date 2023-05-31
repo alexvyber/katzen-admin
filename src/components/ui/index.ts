cadc2ea09a8e0bf6a6d53bec7ff6c3b1966882f3
Alexey Sokolov <alexvyber@gmail.com>
2023-05-31 16:43:12 +0300

    card and separator ui components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,6 @@ export { ButtonLink } from "./button-link"
 export { Button } from "./button"
 export { Container } from "./container"
 export { Loading } from "./loading"
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card"
+
+export { Separator } from "./separator"
