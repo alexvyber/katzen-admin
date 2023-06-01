ca587868bb158a9219c77841a9a5eeafab6ab9b1
Alexey Sokolov <alexvyber@gmail.com>
2023-06-02 00:07:18 +0300

    add global configs

---
+++ src/configs/layout.ts
@@ -0,0 +1,12 @@
+export const screens = {
+  "3xs": "320px",
+  "2xs": "384px",
+  xs: "512px",
+  sm: "640px",
+  md: "768px",
+  "2md": "896px",
+  lg: "1024px",
+  "2lg": "1152px",
+  xl: "1280px",
+  "2xl": "1536px",
+} as const
---
+++ src/configs/theme-config.ts
@@ -0,0 +1,8 @@
+export const themeConfig = {
+  app: {
+    name: "Katzen Admin",
+  },
+  layout: {
+    darkMode: false,
+  },
+}
