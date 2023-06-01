33ce78adb6a2b5ca5c28f6af9773e5a0a2f94230
Alexey Sokolov <alexvyber@gmail.com>
2023-06-02 00:06:26 +0300

    add mock data

---
+++ src/mock/data.ts
@@ -0,0 +1,66 @@
+import User1 from "@/assets/images/users/user.png"
+import User2 from "@/assets/images/users/user2.png"
+import User3 from "@/assets/images/users/user3.png"
+import User4 from "@/assets/images/users/user4.png"
+
+export const messages = [
+  {
+    title: "Wade Warren",
+    desc: "Hi! How are you doing?.....",
+    active: true,
+    hasnotifaction: true,
+    notification_count: 1,
+    image: User1,
+    link: "#",
+  },
+  {
+    title: "Savannah Nguyen",
+    desc: "Hi! How are you doing?.....",
+    active: false,
+    hasnotifaction: false,
+    image: User2,
+    link: "#",
+  },
+  {
+    title: "Ralph Edwards",
+    desc: "Hi! How are you doing?.....",
+    active: false,
+    hasnotifaction: true,
+    notification_count: 8,
+    image: User3,
+    link: "#",
+  },
+  {
+    title: "Cody Fisher",
+    desc: "Hi! How are you doing?.....",
+    active: true,
+    hasnotifaction: false,
+    image: User4,
+    link: "#",
+  },
+  {
+    title: "Savannah Nguyen",
+    desc: "Hi! How are you doing?.....",
+    active: false,
+    hasnotifaction: false,
+    image: User2,
+    link: "#",
+  },
+  {
+    title: "Ralph Edwards",
+    desc: "Hi! How are you doing?.....",
+    active: false,
+    hasnotifaction: true,
+    notification_count: 8,
+    image: User3,
+    link: "#",
+  },
+  {
+    title: "Cody Fisher",
+    desc: "Hi! How are you doing?.....",
+    active: true,
+    hasnotifaction: false,
+    image: User4,
+    link: "#",
+  },
+]
