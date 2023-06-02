c82109eabeea497689761c7aca6d554716df1386
Alexey Sokolov <alexvyber@gmail.com>
2023-06-02 15:46:22 +0300

    update logo to use collapsed

--- src/components/partials/logo.tsx
+++ src/components/partials/logo.tsx
@@ -2,14 +2,16 @@ import { NavLink } from "react-router-dom"
 
 import { Box } from "lucide-react"
 
-export function Logo() {
+export function Logo({ collapsed = false }: { collapsed?: boolean }) {
   return (
     <NavLink to="/dashboard">
-      <div className="flex gap-4 items-center py-6 text-slate-700 dark:bg-slate-800 dark:text-slate-200">
+      <div className="flex gap-4 items-center text-slate-700 dark:bg-slate-800 dark:text-slate-200">
         <Box className="w-7 h-7" />
-        <div>
-          <h2 className="text-xl font-bold">Katzen Admin</h2>
-        </div>
+        {!collapsed && (
+          <div className="hidden md:block">
+            <h2 className="text-xl font-bold">Katzen</h2>
+          </div>
+        )}
       </div>
     </NavLink>
   )
