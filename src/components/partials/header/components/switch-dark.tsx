273f640dbd5a685e3474cb7a051a3d29ab98642d
Alexey Sokolov <alexvyber@gmail.com>
2023-06-04 15:46:37 +0300

    update header component

--- src/components/partials/header/components/switch-dark.tsx
+++ src/components/partials/header/components/switch-dark.tsx
@@ -1,19 +1,15 @@
 import { Icons } from "@/components/icons"
+import { useDarkmode } from "@/hooks/use-dark-mode"
 
 export function SwitchDark() {
-  const [
-    isDark,
-    // setDarkMode
-  ] = [false, undefined]
+  const [isDark, setDarkMode] = useDarkmode()
 
   return (
-    <span>
-      <div
-        className="flex flex-col justify-center items-center text-lg rounded-full cursor-pointer lg:w-8 lg:h-8 dark:text-white text-slate-900 lg:bg-slate-100 lg:dark:bg-slate-900"
-        // onClick={() => setDarkMode(!isDark)}
-      >
-        {isDark ? <Icons icon="heroicons-outline:sun" /> : <Icons icon="heroicons-outline:moon" />}
-      </div>
-    </span>
+    <div
+      className="flex flex-col justify-center items-center text-lg text-gray-900 rounded-full cursor-pointer lg:w-8 lg:h-8 lg:bg-gray-100 dark:text-white lg:dark:bg-gray-900"
+      onClick={() => setDarkMode(!isDark)}
+    >
+      {isDark ? <Icons icon="heroicons-outline:sun" /> : <Icons icon="heroicons-outline:moon" />}
+    </div>
   )
 }
--- src/components/partials/header/index.tsx
+++ src/components/partials/header/index.tsx
@@ -1,85 +1,67 @@
 import { cx } from "cvax"
-
 import { Language } from "./components/language"
 import { Messages } from "./components/messages"
 import { SwitchDark } from "./components/switch-dark"
-
-import { useState } from "react"
 import { useWidth } from "@/hooks/use-width"
 import { Icons } from "@/components/icons"
 import { Logo } from "../logo"
-
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui"
 import { UserNav } from "./components/user-nav"
-import { TeamSwitcher } from "./components/team-switcher"
-import { CommandMenu } from "./components/command-menu"
 import { Notifications } from "./components/notifications"
+import { useState } from "react"
+import { menuItems } from "@/configs/menu-items"
+import { ExpandedMenu } from "../sidebar/nav-menu"
+// import { TeamSwitcher } from "./components/team-switcher"
+// import { CommandMenu } from "./components/command-menu"
 
-export function Header({ className }: React.ComponentProps<"header">) {
+export function Header() {
   const { width, breakpoints } = useWidth()
-  const [collapsed, setMenuCollapsed] = useState(false)
-
-  const handleOpenMobileMenu = () => {
-    console.log("Open Mobile menu")
-  }
-
-  const xl = parseInt(breakpoints.xl)
-  const md = parseInt(breakpoints.md)
 
   return (
-    <header className={className}>
-      <div
-        className={cx(
-          "bg-white px-4 dark:bg-slate-800 md:px-6 py-2",
-          "dark:border-b dark:border-slate-700 dark:border-opacity-60"
-        )}
-      >
-        <div className="flex justify-between items-center h-full">
-          <div className="flex gap-2 items-center md:gap-4">
-            {collapsed && width >= xl && (
-              <button
-                className="text-xl dark:text-white text-slate-900"
-                onClick={() => setMenuCollapsed(!collapsed)}
-              >
-                <Icons icon="akar-icons:arrow-right" />
-              </button>
-            )}
+    <div
+      className={cx(
+        "bg-white border-b dark:border-gray-600 dark:border-opacity-60  dark:bg-gray-900 ",
+        "flex w-full items-center px-4",
+        "h-16"
+      )}
+    >
+      <div className="flex justify-between items-center w-full">
+        <div className="flex gap-2 items-center md:gap-4">
+          {/* {width > breakpoints.lg && <TeamSwitcher />} */}
+          {/* {width > breakpoints.lg && <CommandMenu />} */}
+          {width < breakpoints.lg && <MobileNav />}
+          {width < breakpoints.lg && <Logo />}
+        </div>
 
-            {width < xl && <Logo />}
-
-            <TeamSwitcher />
-            <CommandMenu />
-
-            {width < xl && width >= md && (
-              <button
-                className="text-2xl cursor-pointer dark:text-white text-slate-900"
-                onClick={handleOpenMobileMenu}
-              >
-                <Icons icon="heroicons-outline:menu-alt-3" />
-              </button>
-            )}
-
-            <div className="flex-1 w-full md:flex-none md:w-auto">{/* <CommandMenu /> */}</div>
-          </div>
-
-          <div className="flex items-center space-x-3 lg:space-x-6 nav-tools">
-            <Language />
-            <SwitchDark />
-
-            <Notifications />
-            {width >= md && <Messages />}
-            <UserNav />
-
-            {width <= md && (
-              <div
-                className="text-2xl cursor-pointer dark:text-white text-slate-900"
-                onClick={handleOpenMobileMenu}
-              >
-                <Icons icon="heroicons-outline:menu-alt-3" />
-              </div>
-            )}
-          </div>
+        <div className="flex gap-3 items-center">
+          {width >= breakpoints["sm"] && <Language />}
+          <SwitchDark />
+          <Notifications />
+          {width >= breakpoints.md && <Messages />}
+          <UserNav />
         </div>
       </div>
-    </header>
+    </div>
+  )
+}
+
+// REFACTOR: exctract
+const MobileNav = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <button type="button" className="text-2xl text-gray-900 cursor-pointer dark:text-white">
+          <Icons icon="heroicons-outline:menu-alt-2" />
+        </button>
+      </SheetTrigger>
+      <SheetContent position="left" className="w-60 dark:border-gray-600 border-r-gray-200">
+        <div className={"flex pb-1.5  pt-4"}>
+          <Logo />
+        </div>
+        <ExpandedMenu menuItems={menuItems} onItemClick={() => setOpen(false)} />
+      </SheetContent>
+    </Sheet>
   )
 }
