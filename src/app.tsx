12f94cb7958e72f818325b607b7ac281634fa182
Alexey Sokolov <alexvyber@gmail.com>
2023-06-04 15:33:34 +0300

    routes generated on the fly

--- src/app.tsx
+++ src/app.tsx
@@ -6,38 +6,49 @@ import { Layout } from "@/layouts/layout"
 
 import { Loading } from "@/components/ui"
 import { Toaster } from "@/components/ui"
+import { menuItems } from "./configs/menu-items"
 
-const Error404 = lazy(() => import("@/pages/erros/404"))
+const Error404 = lazy(() => import("@/pages/utility-pages/404"))
 
-const routes = {
-  home: lazy(() => import("@/pages/utility/blank")),
-  dashboard: lazy(() => import("@/pages/utility/blank")),
-  chat: lazy(() => import("@/pages/utility/blank")),
-  todo: lazy(() => import("@/pages/utility/blank")),
+// TODO: Rewrite it pre-release
+// NOTE: dynamicly form routes object
+const routes = {} as Record<
+  string,
+  React.LazyExoticComponent<() => JSX.Element> | (() => JSX.Element)
+>
 
-  //
+// TODO: remove pre-release
+menuItems
+  .filter((item) => "link" in item || "path" in item)
+  .forEach((item) => {
+    if ("path" in item) {
+      item.children?.map((child) =>
+        Object.assign(routes, {
+          [`${item.path}/${child.link}`]: child.show
+            ? lazy(() => import(`./pages/${item.path}/${child.link}`))
+            : () => <Navigate to="/utility-pages/blank-page" />,
+        })
+      )
+    }
 
-  "blank-page": lazy(() => import("@/pages/utility/blank")),
-  "access-denied": lazy(() => import("@/pages/utility/access-denied")),
-  "coming-soon": lazy(() => import("@/pages/utility/coming-soon")),
-  "under-construction": lazy(() => import("@/pages/utility/under-construction")),
-  pricing: lazy(() => import("@/pages/utility/pricing")),
-  changelog: lazy(() => import("@/pages/utility/changelog")),
-  faq: lazy(() => import("@/pages/utility/faq")),
-  welcome: lazy(() => import("@/pages/welcome")),
-  "*": () => <Navigate to="/404" />,
-}
+    if ("link" in item) {
+      Object.assign(routes, {
+        [`${item.link}`]: item.show
+          ? lazy(() => import(`./pages/${item.link}`))
+          : () => <Navigate to="/utility-pages/blank-page" />,
+      })
+    }
+  })
 
 export function App() {
   return (
-    <main className="relative h-screen bg-slate-50">
+    <main className="relative h-screen bg-gray-50 dark:bg-gray-800">
       <Routes>
         <Route path="/*" element={<Layout />}>
           {Object.entries(routes).map(([route, Page]) => (
             <Route path={route} element={<Page />} />
           ))}
         </Route>
-
         <Route path="/404" element={withSuspense(Error404)} />
       </Routes>
       <Toaster />
