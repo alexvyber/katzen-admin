6d372b849f3a4a32a631b8544346d5cb422a1e2c
Alexey Sokolov <alexvyber@gmail.com>
2023-06-04 15:47:03 +0300

    update sidebar

--- src/components/partials/sidebar/index.tsx
+++ src/components/partials/sidebar/index.tsx
@@ -1,21 +1,59 @@
-import { ScrollArea } from "@/components/ui"
-import { MenuItemsVertical } from "@/configs/menu-items"
-import { Navmenu } from "./nav-menu"
+import { ScrollArea, Viewport } from "@/components/ui"
+import { menuItems } from "@/configs/menu-items"
+import { ItemTrigger, Navmenu, triggerVariants } from "./nav-menu"
 import { cx } from "cvax"
 import { Logo } from "../logo"
-import { useState } from "react"
+import useMenuCollapsed from "@/hooks/use-menu-collapsed"
+import { Icons } from "@/components/icons"
+import { memo } from "react"
 
-export function Sidebar({ className }: React.ComponentProps<"div">) {
-  const [collapsed] = useState(false)
+const Sidebar = memo(function Sidebar() {
+  const [collapsed, setCollapsed] = useMenuCollapsed()
 
   return (
-    <div className={cx("", className)}>
-      <ScrollArea className={cx("h-screen bg-white", collapsed ? "w-16 px-2" : "w-60 px-4")}>
+    <ScrollArea className={cx("h-screen ", collapsed ? "w-20 px-2" : "w-60 px-4", "z-50")}>
+      <Viewport className="flex flex-col gap-6">
         <div className={cx("flex pb-1.5  pt-4", collapsed && "justify-center")}>
           <Logo collapsed={collapsed} />
         </div>
-        <Navmenu menuItems={MenuItemsVertical} collapsed={collapsed} />
-      </ScrollArea>
-    </div>
+
+        <Navmenu menuItems={menuItems} collapsed={collapsed} />
+
+        <CollapseButton collapsed={collapsed} setCollapsed={setCollapsed} />
+      </Viewport>
+    </ScrollArea>
+  )
+})
+
+// REFACTOR
+function CollapseButton({
+  collapsed,
+  setCollapsed,
+}: { collapsed: boolean; setCollapsed: ReturnType<typeof useMenuCollapsed>[1] }) {
+  return collapsed ? (
+    <button
+      className={cx(triggerVariants(), "p-0 group  cursor-pointer relative")}
+      onClick={() => setCollapsed(!collapsed)}
+    >
+      <Icons icon="lucide:maximize" className="w-6 h-6" />
+    </button>
+  ) : (
+    <button
+      className={cx(
+        "flex cursor-pointer rounded-lg  text-sm font-semibold capitalize text-gray-600 dark:text-gray-300",
+        "data-[state='open']:bg-secondary-500 data-[state='open']:bg-opacity-20",
+        triggerVariants()
+      )}
+      onClick={() => setCollapsed(!collapsed)}
+    >
+      <ItemTrigger
+        item={{
+          title: "Collapse",
+          icon: "lucide:minimize",
+        }}
+      />
+    </button>
   )
 }
+
+export { Sidebar, Sidebar as default }
--- src/components/partials/sidebar/nav-menu.tsx
+++ src/components/partials/sidebar/nav-menu.tsx
@@ -1,3 +1,5 @@
+// REFACTOR:
+
 import { cn, cvax, cx } from "cvax"
 
 import { NavLink } from "react-router-dom"
@@ -8,8 +10,7 @@ import {
   AccordionTrigger,
   Separator,
 } from "@/components/ui"
-import { MenuItemVertical } from "@/configs/menu-items"
-import { isItemBasic, isItemHeader, isItemWithChildren } from "@/configs/menu-utils"
+
 import { Icons } from "@/components/icons"
 
 import {
@@ -19,175 +20,185 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from "@/components/ui/menubar"
-import { MenuItem } from "@/configs/menu-types"
+import { isItemBasic, isItemWithChildren } from "@/configs/menu-utils"
+import { MenuItemBasic, MenuItems } from "@/configs/menu-types"
 
-const triggerVariants = cvax({
-  base: "flex gap-3 justify-center w-full items-center text-sm font-semibold capitalize rounded-lg text-slate-600 dark:text-slate-300 px-2 py-2",
+export const triggerVariants = cvax({
+  base: "flex gap-3 justify-center w-full items-center text-sm font-semibold capitalize rounded-lg text-gray-600 dark:text-gray-300 px-2 py-2",
   variants: {},
 })
 
 export function Navmenu({
   menuItems,
   collapsed = false,
-}: { menuItems: MenuItemVertical[]; collapsed?: boolean }) {
-  return (
-    <>
-      {!collapsed ? (
-        <Accordion type="single" collapsible className="flex flex-col">
-          {menuItems.map((item, index) => {
-            if (isItemHeader(item)) {
-              return (
-                <div
-                  key={`${item.title}-${index}`}
-                  className="flex items-center h-9 text-xs font-semibold uppercase text-slate-800 dark:text-slate-300"
-                >
-                  {item.title}
-                </div>
-              )
-            }
-
-            if (isItemBasic(item)) {
-              return <ItemBasic key={`${item.link}-${index}`} item={item} />
-            }
-
-            if (isItemWithChildren(item)) {
-              if (!collapsed)
-                return (
-                  <AccordionItem
-                    key={`${item.title}-${index}`}
-                    value={index.toString()}
-                    className="border-none"
-                  >
-                    <AccordionTrigger
-                      className={cx(
-                        "flex cursor-pointer rounded-lg  text-sm font-semibold capitalize text-slate-600 dark:text-slate-300",
-                        "data-[state='open']:bg-secondary-500 data-[state='open']:bg-opacity-20",
-                        triggerVariants()
-                      )}
-                    >
-                      <ItemTrigger item={item} />
-                    </AccordionTrigger>
-                    <AccordionContent className="pb-3">
-                      {item.children?.map((subMenuItem, index) => (
-                        <NavLink
-                          to={subMenuItem.link}
-                          key={`${subMenuItem.link}-${index}`}
-                          className="block pr-1 pl-3 mb-1 first:mt-3"
-                        >
-                          {({ isActive }) => (
-                            <div
-                              className={cx(
-                                "flex items-center gap-3 text-sm",
-                                isActive
-                                  ? " text-black font-medium dark:text-white"
-                                  : "text-slate-600 dark:text-slate-300"
-                              )}
-                            >
-                              <span
-                                className={cn(
-                                  "inline-block h-2.5 w-2.5 flex-none rounded-full border-2  dark:border-white",
-                                  isActive
-                                    ? "dark:bg-slate-300 border-slate-600 bg-slate-600"
-                                    : "border-slate-300"
-                                )}
-                              />
-                              <span className="flex-1">{subMenuItem.title}</span>
-                            </div>
-                          )}
-                        </NavLink>
-                      ))}
-                    </AccordionContent>
-                  </AccordionItem>
-                )
-
-              if (collapsed) return null
-            }
-
-            // TODO: Remove dev time
-            throw new Error("Should be unreacheable")
-          })}
-        </Accordion>
-      ) : (
-        <Menubar className="flex flex-col gap-3 items-start border-none">
-          {menuItems.map((item, index) => {
-            if (isItemHeader(item)) {
-              return <Separator className="my-2" />
-            }
-
-            if (isItemBasic(item)) {
-              return (
-                <NavLink
-                  key={`${item.title}-${index}`}
-                  className={triggerVariants()}
-                  to={item.link}
-                >
-                  <ItemIcon icon={item.icon} />
-                </NavLink>
-              )
-            }
-
-            if (isItemWithChildren(item)) {
-              return (
-                <MenubarMenu key={`${item.title}-${index}`}>
-                  <MenubarTrigger className={cx(triggerVariants(), "p-0")}>
-                    <ItemIcon icon={item.icon} />
-                  </MenubarTrigger>
-                  <MenubarContent
-                    className="absolute left-14"
-                    style={{ top: `-${item.children.length * 20}px` }}
-                  >
-                    {item.children?.map((subMenuItem, index) => (
-                      <MenubarItem
-                        asChild
-                        key={`${subMenuItem.link}-${index}`}
-                        className="block cursor-pointer"
-                      >
-                        <NavLink to={subMenuItem.link}>
-                          {({ isActive }) => (
-                            <div
-                              className={cx(
-                                "flex items-center gap-3 text-sm",
-                                isActive
-                                  ? " text-black font-medium dark:text-white"
-                                  : "text-slate-600 dark:text-slate-300"
-                              )}
-                            >
-                              <span
-                                className={cn(
-                                  "inline-block h-2.5 w-2.5 flex-none rounded-full border-2  dark:border-white",
-                                  isActive
-                                    ? "dark:bg-slate-300 border-slate-600 bg-slate-600"
-                                    : "border-slate-300"
-                                )}
-                              />
-                              <span className="flex-1">{subMenuItem.title}</span>
-                            </div>
-                          )}
-                        </NavLink>
-                      </MenubarItem>
-                    ))}
-                  </MenubarContent>
-                </MenubarMenu>
-              )
-            }
-
-            // TODO: Remove dev time
-            throw new Error("Should be unreacheable")
-          })}
-        </Menubar>
-      )}
-    </>
+}: { menuItems: MenuItems; collapsed?: boolean }) {
+  return !collapsed ? (
+    <ExpandedMenu menuItems={menuItems} />
+  ) : (
+    <CollapsedMenu menuItems={menuItems} />
   )
 }
 
+export function ExpandedMenu({
+  menuItems,
+  onItemClick,
+}: { menuItems: MenuItems; onItemClick?: () => void }) {
+  return (
+    <Accordion type="single" collapsible className="flex flex-col">
+      {menuItems.map((item, index) => {
+        if (!("link" in item || "path" in item)) {
+          return (
+            <div
+              key={`${item.title}-${index}`}
+              className="flex items-center h-9 text-xs font-semibold text-gray-800 uppercase dark:text-gray-300"
+            >
+              {item.title}
+            </div>
+          )
+        }
+
+        if (isItemBasic(item)) {
+          return (
+            <button type="button" onClick={onItemClick}>
+              <ItemBasic key={`${item.link}-${index}`} item={item} />
+            </button>
+          )
+        }
+
+        if (isItemWithChildren(item)) {
+          return (
+            <AccordionItem
+              key={`${item.title}-${index}`}
+              value={index.toString()}
+              className="border-none"
+            >
+              <AccordionTrigger
+                className={cx(
+                  "flex cursor-pointer rounded-lg  text-sm font-semibold capitalize text-gray-600 dark:text-gray-300",
+                  "data-[state='open']:bg-secondary-500 data-[state='open']:bg-opacity-20",
+                  triggerVariants()
+                )}
+              >
+                <ItemTrigger item={item} />
+              </AccordionTrigger>
+              <AccordionContent className="pb-3">
+                {item.children?.map((subMenuItem, index) => (
+                  <NavLink
+                    to={`${item.path}/${subMenuItem.link}`}
+                    key={`${subMenuItem.link}-${index}`}
+                    className="block pr-1 pl-3 mb-1 first:mt-3"
+                    onClick={onItemClick}
+                  >
+                    {({ isActive }) => (
+                      <div
+                        className={cx(
+                          "flex items-center gap-3 text-sm",
+                          isActive
+                            ? " text-black font-medium dark:text-white"
+                            : "text-gray-600 dark:text-gray-300"
+                        )}
+                      >
+                        <span
+                          className={cn(
+                            "inline-block h-2.5 w-2.5 flex-none rounded-full border-2  dark:border-white",
+                            isActive
+                              ? "dark:bg-gray-300 border-gray-600 bg-gray-600"
+                              : "border-gray-300"
+                          )}
+                        />
+                        <span className="flex-1">{subMenuItem.title}</span>
+                      </div>
+                    )}
+                  </NavLink>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          )
+        }
+
+        never(item)
+      })}
+    </Accordion>
+  )
+}
+
+function CollapsedMenu({ menuItems }: { menuItems: MenuItems }) {
+  return (
+    <Menubar className="flex flex-col gap-3 items-start bg-transparent border-none">
+      {menuItems.map((item, index) => {
+        if (!("link" in item || "path" in item)) {
+          return <Separator className="my-2" />
+        }
+
+        if (isItemBasic(item)) {
+          return (
+            <NavLink key={`${item.title}-${index}`} className={triggerVariants()} to={item.link}>
+              <ItemIcon icon={item.icon} />
+            </NavLink>
+          )
+        }
+
+        if (isItemWithChildren(item)) {
+          return (
+            <MenubarMenu key={`${item.title}-${index}`}>
+              <MenubarTrigger
+                className={cx(triggerVariants(), "p-0 group  cursor-pointer relative")}
+              >
+                <ItemIcon icon={item.icon} />
+                <Icons
+                  icon="lucide:chevron-right"
+                  className="absolute right-0 flex-none w-3.5 h-3.5 text-gray-500 group-hover:scale-125"
+                />
+              </MenubarTrigger>
+              <MenubarContent
+                className="absolute left-18"
+                style={{ top: `-${item.children.length * 20}px` }}
+              >
+                {item.children?.map((subMenuItem, index) => (
+                  <MenubarItem
+                    asChild
+                    key={`${subMenuItem.link}-${index}`}
+                    className="block cursor-pointer"
+                  >
+                    <NavLink to={`${item.path}/${subMenuItem.link}`}>
+                      {({ isActive }) => (
+                        <div
+                          className={cx(
+                            "flex items-center gap-3 text-sm",
+                            isActive
+                              ? " text-black font-medium dark:text-white"
+                              : "text-gray-600 dark:text-gray-300"
+                          )}
+                        >
+                          <span
+                            className={cn(
+                              "inline-block h-2.5 w-2.5 flex-none rounded-full border-2  dark:border-white",
+                              isActive
+                                ? "dark:bg-gray-300 border-gray-600 bg-gray-600"
+                                : "border-gray-300"
+                            )}
+                          />
+                          <span className="flex-1">{subMenuItem.title}</span>
+                        </div>
+                      )}
+                    </NavLink>
+                  </MenubarItem>
+                ))}
+              </MenubarContent>
+            </MenubarMenu>
+          )
+        }
+        never(item)
+      })}
+    </Menubar>
+  )
+}
+
+// REFACTOR: extract
 function ItemBasic({
   item,
 }: {
-  item: Required<
-    {
-      link: string
-    } & MenuItem
-  >
+  item: MenuItemBasic
 }) {
   return (
     <NavLink className={triggerVariants()} to={item.link}>
@@ -196,24 +207,30 @@ function ItemBasic({
   )
 }
 
+// REFACTOR: extract
 function ItemIcon({ icon }: { icon: string }) {
   return (
-    <span className="inline-flex flex-grow-0 items-center text-lg text-slate-600 dark:text-slate-300">
+    <span className="inline-flex flex-grow-0 items-center text-lg text-gray-600 dark:text-gray-300">
       <Icons icon={icon} className="w-6 h-6" />
     </span>
   )
 }
 
-function ItemTrigger({ item }: { item: any }) {
+// REFACTOR: extract
+export function ItemTrigger({ item }: { item: { title: string; icon: string; badge?: string } }) {
   return (
     <div className="flex flex-1 gap-2 items-center">
       <ItemIcon icon={item.icon} />
       <div className="flex-grow text-start">{item.title}</div>
       {item.badge && (
-        <span className="inline-flex py-1 px-2 text-xs font-semibold align-baseline whitespace-nowrap rounded-md bg-slate-900 text-slate-50 dark:bg-slate-700 dark:text-slate-300">
+        <span className="inline-flex py-1 px-2 text-xs font-semibold text-gray-50 align-baseline whitespace-nowrap bg-gray-900 rounded-md dark:text-gray-300 dark:bg-gray-700">
           {item.badge}
         </span>
       )}
     </div>
   )
+}
+
+function never(arg: never): asserts arg is never {
+  throw new Error(`arg must be type of never, got: ${typeof arg}`)
 }
