383f5654ed28c186e2cfadcf7cea0da9bdb9a0c1
Alexey Sokolov <alexvyber@gmail.com>
2023-06-04 15:33:48 +0300

    logo edits

--- src/components/partials/logo.tsx
+++ src/components/partials/logo.tsx
@@ -5,13 +5,9 @@ import { Box } from "lucide-react"
 export function Logo({ collapsed = false }: { collapsed?: boolean }) {
   return (
     <NavLink to="/dashboard">
-      <div className="flex gap-4 items-center text-slate-700 dark:bg-slate-800 dark:text-slate-200">
+      <div className="flex gap-4 items-center text-gray-700 dark:text-gray-200">
         <Box className="w-7 h-7" />
-        {!collapsed && (
-          <div className="hidden md:block">
-            <h2 className="text-xl font-bold">Katzen</h2>
-          </div>
-        )}
+        {!collapsed && <h2 className="text-xl font-bold text-gray-900 dark:text-white">Katzen</h2>}
       </div>
     </NavLink>
   )
